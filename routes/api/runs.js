0012bc6b1f861748069ef8987c9d600757bff0c5
AntTal13 <anthonytalarico13@gmail.com>
2023-05-23 09:08:52 -0400

    ensureLoggedIn

--- routes/api/runs.js
+++ routes/api/runs.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const runsCtrl = require('../../controllers/api/runs');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
 // GET /runs 
 router.get('/', runsCtrl.index);
 
 // POST /runs 
-router.post('/', runsCtrl.addRun);
+router.post('/', ensureLoggedIn, runsCtrl.addRun);
 
 // GET /runs/user
-router.get('/user', runsCtrl.forUser);
+router.get('/user', ensureLoggedIn, runsCtrl.forUser);
 
 // GET /runs/:id
-router.get('/:id', runsCtrl.show);
+router.get('/:id', ensureLoggedIn, runsCtrl.show);
 
 // DELETE /runs/:id
-router.delete('/:id', runsCtrl.delete);
+router.delete('/:id', ensureLoggedIn, runsCtrl.delete);
 
 
 module.exports = router;
